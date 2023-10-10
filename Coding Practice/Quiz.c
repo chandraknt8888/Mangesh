959cad02880e60964de473e22a9b84409d93de6f
Chandrakant Gore <cgore707070@gmail.com>
2023-10-10 23:08:04 +0530

    first commit

---
+++ Coding Practice/Quiz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+main()
+{
+	/*int number;
+	printf("Enter A Number\t");
+	scanf("%d", &number);
+	printf("The Number Entered is %d", number);*/
+
+	/*int number1, number2, number3;
+	printf("Enter the number \t");
+	scanf("%d %d", &number1, &number2);
+	number3 = number1 + number2;
+	printf("The Sum is %d : ", number3);*/
+
+	// using size of oprators
+
+	/*printf("Character takes %zd bytes in memory\n", sizeof(char));
+	printf("Integer takes %zd bytes in memory\n", sizeof(int));
+	printf("Float takes %zd bytes in mrmory\n", sizeof(float));
+	printf("Long takes %zd bytes in memory\n", sizeof(long));
+	printf("Double takes %zd bytes in memory\n", sizeof(double));*/
+
+	/*int a = 3251;
+	unsigned b = 2568;
+	printf("%d %d", a, b);*/
+
+	//char a = 128;
+	//unsigned char b = 874422;
+	//printf("%d\n", b);
+
+	/*float a = 3.5e38;
+	double b = 3.5e30;
+	printf("%f %lf", a,b);*/
+
+
+}
