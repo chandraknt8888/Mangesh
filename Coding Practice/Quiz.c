035ef710954dafde95ed87165555aefd0c038edb
Chandrakant Gore <cgore707070@gmail.com>
2023-11-21 09:21:02 +0530

    collage

--- Coding Practice/Quiz.c
+++ Coding Practice/Quiz.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-main()
+#include<limits.h>
+
+int main()
 {
 	/*int number;
 	printf("Enter A Number\t");
@@ -31,5 +33,64 @@ main()
 	/*float a = 3.5e38;
 	double b = 3.5e30;
 	printf("%f %lf", a,b);*/
+
+
+		// when we add maximum value to integers then it rounds of to its minimum value (circle) 
+
+		//printf("%d\n", INT_MIN);
+		//printf("%d\n", INT_MAX);
+		//// printf("%d\n", INT_MAX + 1);
+		//// printf("%d\n", INT_MAX + 2);
+		//// printf("%d\n", INT_MAX + 3);
+		//// printf("%d\n", INT_MAX + 4);
+		//// printf("%d\n", INT_MAX + 5);
+		//// printf("%d\n", INT_MAX + 6);
+		//// printf("%d\n", INT_MAX + 7);
+
+		//// when we add maximum value to integers then it rounds of to its minimum value (circle) 
+		//printf("%d\n", INT_MIN - 1);
+		//printf("%d\n", INT_MIN - 2);
+		//printf("%d\n", INT_MIN - 3);
+		//printf("%d\n", INT_MIN - 4);
+		//printf("%d\n", INT_MIN - 5);
+
+	
+	//int a = 10;
+	//int* p;
+	//p = &a; // &a = address of address
+	//printf("%d\n", p);
+	//printf("%d\n", *p);
+	//printf("%d\n", &a);
+
+	/*int a = 4;
+
+	scanf("%d", &a);
+	int arr[a];
+
+	for (int i = 0; i < a; i++)
+	{
+		scanf("%d", &arr[i]);
+		if (i % 2 == 0)
+		{
+			printf("%d\n", arr[i]);
+		}
+	}*/
+
+
+	int i, j;
+
+	for (i = 1; i <= 5; i++) // column 
+	{
+		for (j = 1; j <= 5; j++) // row
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+
+
+	return 0;
+
 
 }
